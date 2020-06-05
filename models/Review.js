25822792667f75bcef6e40b5ce492841746c49be
Mike <mike.nichols97@gmail.com>
2020-06-05 03:53:10 -0400

    Back-end getting reviews from the db

---
+++ models/Review.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+    title: String,
+    jeff: Number,
+    jack: Number,
+    trill: Number,
+    audience: Number,
+    butter: Number,
+    director: String,
+    genre: String,
+    subGenre: String,
+    release: String,
+    runtime: Number,
+    podcast: Number
+})
+
+module.exports = Review = mongoose.model('Movies', ReviewSchema, 'movies');
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-//const Review = require('./models/Review');
+const Review = require('../models/Review');
 
 router.get("/", (req, res) => {
-    res.send("WORKING");
+    Review.find()
+    .then(reviews => res.json(reviews))
+    .catch(err => res.json(err))
 })
 
 module.exports = router;
