bb497817e058a3c7df17d731db57b0bbab13cd27
Mike <mike.nichols97@gmail.com>
2020-06-05 06:15:15 -0400

    Basic review table. match-sorter package added for cleaner searching

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import matchSorter, {rankings, caseRankings} from 'match-sorter';
 
 import ReviewList from './ReviewList.js';
 
@@ -11,16 +12,14 @@ class Home extends React.Component {
     }
 
     componentDidMount = () => {
-        axios.get('/api/reviews/test')
+        axios.get('/api/reviews')
         .then(res => {
             this.setState({
                 reviews: res.data
             })
         })
         .catch(res => {
-            this.setState({
-                reviews: "Error connecting to DB"
-            })
+            //alert("Error connecting to DB")
         })
     }
 
@@ -31,11 +30,11 @@ class Home extends React.Component {
     }
 
     render() {
-        console.log(this.state.reviews)
+        const reviews = this.state.query === "" ? this.state.reviews : matchSorter(this.state.reviews, this.state.query, {keys: ['Title']})
         return (
             <div id="home">
                 <input onChange={this.onChange} type="text" value={this.state.query} placeholder="Start entering a movie title here" />
-                <ReviewList query={this.state.query} />
+                <ReviewList reviews={reviews} />
             </div>
         )
     }
--- client/src/components/ReviewList.js
+++ client/src/components/ReviewList.js
@@ -3,7 +3,32 @@ import { Link } from 'react-router-dom';
 
 const ReviewList = props => {
   return (
-    <h1>{props.query}</h1>
+    <table id="reviews">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Jeff</th>
+                <th>KenJac</th>
+                <th>Trill</th>
+                <th>Audience</th>
+                <th className="butter">Butter Score</th>
+            </tr>
+        </thead>
+        <tbody>
+            {props.reviews.map(review => {
+                return (
+                    <tr key={review._id}>
+                        <td className="review-title">{review.Title}</td>
+                        <td className="review-score">{review.Jeff}</td>
+                        <td className="review-score">{review.Jack}</td>
+                        <td className="review-score">{review.Trill}</td>
+                        <td className="review-score">{review["Audience (LCB)"]}</td>
+                        <td className="review-score butter">{review["Butter Score"]}</td>
+                    </tr>
+                )
+            })}
+        </tbody>
+    </table>
   );
 }
 
