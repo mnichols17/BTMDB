b5bb8d08072d07db28282f5b86f9deab84ad4a46
Mike <mike.nichols97@gmail.com>
2020-06-05 04:53:03 -0400

    Added basic homepage w/ searchbar and connected to back-end

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+import ReviewList from './ReviewList.js';
 
 class Home extends React.Component {
 
     state = {
-        reviews: []
+        reviews: [],
+        query: ""
+    }
+
+    componentDidMount = () => {
+        axios.get('/api/reviews/test')
+        .then(res => {
+            this.setState({
+                reviews: res.data
+            })
+        })
+        .catch(res => {
+            this.setState({
+                reviews: "Error connecting to DB"
+            })
+        })
+    }
+
+    onChange = e => {
+        this.setState({
+            query: e.target.value
+        })
     }
 
     render() {
+        console.log(this.state.reviews)
         return (
-            <div>
-                <h1>Test</h1>
+            <div id="home">
+                <input onChange={this.onChange} type="text" value={this.state.query} placeholder="Start entering a movie title here" />
+                <ReviewList query={this.state.query} />
             </div>
         )
     }
---
+++ client/src/components/ReviewList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ReviewList = props => {
+  return (
+    <h1>{props.query}</h1>
+  );
+}
+
+export default ReviewList;
